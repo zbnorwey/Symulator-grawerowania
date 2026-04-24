38ebde3573ecb140d6318c306082e05cca726be1
norwey <zb.norwey@gmail.com>
2026-04-24 10:40:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('fetch', (event) => {
   // Prosty mechanizm pass-through dla PWA
 });
+// Zamiast zwykłego update(), poczekaj na załadowanie czcionek
+document.fonts.ready.then(function () {
+  update();
+});
